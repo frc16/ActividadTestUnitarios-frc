dfce397e3775db9e7dfc38f2979eb33c3949d4b3
frc16 <153406170+frc16@users.noreply.github.com>
2026-01-22 10:17:53 +0100

    1

---
+++ src/test/java/es/fplumara/dam1/facturacion/FacturacionTest.java
@@ -0,0 +1,4 @@
+package es.fplumara.dam1.facturacion;
+
+public class FacturacionTest {
+}
--- src/test/java/es/fplumara/dam1/operaciones/OperacionesTest.java
+++ src/test/java/es/fplumara/dam1/operaciones/OperacionesTest.java
@@ -1,8 +1,9 @@
 package es.fplumara.dam1.operaciones;
-
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -10,13 +11,29 @@ public class OperacionesTest {
 
     @ParameterizedTest
     @CsvSource({
-            "1,INSUFICIENTE",
-            "7.3,INSUFICIENTE"
+            "0.0,INSUFICIENTE",
+            "4.99,INSUFICIENTE",
+            "5.0, APROBADO",
+            "6.99, APROBADO",
+            "7.0, NOTABLE",
+            "8.99, NOTABLE",
+            "9.0, SOBRESALIENTE",
+            "10.0, SOBRESALIENTE",
     })
     @DisplayName("Comprueba las notas")
     public void clasificacionNotasValidas(double nota, String resultadoEsperado) {
         String resultado = Operaciones.calificacion(nota);
         assertEquals(resultadoEsperado,resultado);
     }
+    @ParameterizedTest
+    @ValueSource(doubles=-0.01,10.01})
+    @DisplayName("comprobar IllegalArgumentException para notas fuera del rango")
+
+    public void clasificacionDeNotasInvalidas(double notaInvalida){
+
+    }
+
+
 }
+
 
